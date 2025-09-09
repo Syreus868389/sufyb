074163363a93e2ec51a825c7b4a8d53749b67ed9
Théo Pavlovic <pavlovic.theo@gmail.com>
2025-09-09 16:26:26 +0000

    fixed dev port exposure

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,18 +1,37 @@
 // @ts-check
-import { defineConfig } from 'astro/config'
+import { defineConfig, fontProviders } from 'astro/config';
 
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 
 import icon from 'astro-icon';
 
-
-import sitemap from '@astrojs/sitemap';
-
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  experimental: {
+    fonts: [{
+      provider: fontProviders.google(),
+      name: "Lora",
+      cssVariable: "--font-lora"
+    },
+    {
+      provider: fontProviders.fontsource(),
+      name: "Source Sans Pro",
+      cssVariable: "--font-source"
+    }]
+  },
+  site: 'https://lesmotsenfilature.fr',
+    redirects: {
+    "/accueil": "/"
+  },
   vite: {
-    plugins: [tailwindcss()],
+    // @ts-expect-error
+    plugins: [tailwindcss()]
+  },
+
+  image:{
+    responsiveStyles: true,
   },
 
   integrations: [sitemap()],
